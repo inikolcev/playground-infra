1ba203726107fbaea262caaf0b8f5427efe6b1fc
Ivan Nikolchev <inikolch@redhat.com>
2022-03-04 23:16:26 +0100

    initial commit

---
+++ eks.tf
@@ -0,0 +1,111 @@
+resource "aws_iam_role" "eks-cluster" {
+  name = "eks-cluster"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
+
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = "eks-cluster"
+  role_arn = aws_iam_role.eks-cluster.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.private-eu-1a.id,
+      aws_subnet.private-eu-1b.id,
+      aws_subnet.public-eu-1a.id,
+      aws_subnet.public-eu-1b.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy]
+}
+
+resource "aws_iam_role" "nodes" {
+  name = "eks-node-group"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_eks_node_group" "private-nodes" {
+  cluster_name    = aws_eks_cluster.eks-cluster.name
+  node_group_name = "private-nodes"
+  node_role_arn   = aws_iam_role.nodes.arn
+
+  subnet_ids = [
+    aws_subnet.private-eu-1a.id,
+    aws_subnet.private-eu-1b.id
+  ]
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t2.medium"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 5
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  labels = {
+    role = "general"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "null_resource" "kubectl" {
+   depends_on = [aws_eks_cluster.eks-cluster]
+   provisioner "local-exec" {
+      command = "aws eks --region eu-central-1 update-kubeconfig --name eks-cluster"
+      }
+}
---
+++ network.tf
@@ -0,0 +1,124 @@
+#
+# VPC
+
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+#
+# Internet Gateway
+#
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+}
+
+#
+# Subnets
+#
+resource "aws_subnet" "private-eu-1a" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.0.0/19"
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    "name"                              = "private-eu-1a"
+    "kubernetes.io/role/internal-elb"   = "1"
+    "kubernetes.io/cluster/eks-cluster" = "owned"
+  }
+}
+
+resource "aws_subnet" "private-eu-1b" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.32.0/19"
+  availability_zone = "eu-central-1b"
+
+  tags = {
+    "name"                              = "private-eu-1b"
+    "kubernetes.io/role/internal-elb"   = "1"
+    "kubernetes.io/cluster/eks-cluster" = "owned"
+  }
+}
+
+resource "aws_subnet" "public-eu-1a" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.64.0/19"
+  availability_zone       = "eu-central-1a"
+
+  tags = {
+    "name"                              = "public-eu-1a"
+    "kubernetes.io/role/elb"            = "1"
+    "kubernetes.io/cluster/eks-cluster" = "owned"
+  }
+}
+
+resource "aws_subnet" "public-eu-1b" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.96.0/19"
+  availability_zone       = "eu-central-1b"
+
+  tags = {
+    "name"                       = "public-eu-1b"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
+}
+
+#
+# NAT Gateway
+#
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "eu-1a" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-eu-1a.id
+
+  depends_on = [aws_internet_gateway.default]
+}
+
+# 
+# Route Tables
+#
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+
+      cidr_block                 = "0.0.0.0/0"
+      nat_gateway_id             = aws_nat_gateway.eu-1a.id
+    }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+    
+      cidr_block                 = "0.0.0.0/0"
+      gateway_id                 = aws_internet_gateway.default.id
+    }
+}
+
+#
+# Route Table Association
+#
+resource "aws_route_table_association" "private-eu-1a" {
+  subnet_id      = aws_subnet.private-eu-1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-eu-1b" {
+  subnet_id      = aws_subnet.private-eu-1b.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public-eu-1a" {
+  subnet_id      = aws_subnet.public-eu-1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-eu-1b" {
+  subnet_id      = aws_subnet.public-eu-1b.id
+  route_table_id = aws_route_table.public.id
+}
